6b428bd2ec41ddbf159dcc6a05d8807fdeca998a
sreelekha-2 <89189294+sreelekha-2@users.noreply.github.com>
2022-09-09 15:52:06 +0530

    1 file modified

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ console.log('Hello!');
 const arr = [1, 2, 3, 2, 2, 4, 5, 1, 4];
 const filteredArr = arr.filter((ele, i) => i === arr.indexOf(ele));
 console.log(filteredArr);
+
+const arr2 = [3, 3, 4, 5, 6, 6, 7, 8, 9];
+
+const newArr = arr2.filter((ele, index) => index === arr2.indexOf(ele));
+console.log(newArr);
